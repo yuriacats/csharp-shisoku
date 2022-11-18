71be579b620abc4f54c9e05cc01c6686ececd81a
yuriacats <yuria.soft0615@gmail.com>
2022-11-18 13:20:06 +0000

    [add]Make MojiretsuPaser

--- shisoku/Program.cs
+++ shisoku/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+Console.WriteLine("hello");
+
+static (int,int) hoge(String input){
+    int number_num=0;
+    int number = 0;
+    foreach (char i in input){
+        int digit = i - '0';
+        if (0 <= digit && digit < 10){
+            number = number* 10 + digit;
+            number_num++;
+        }else{
+            break;
+        }
+    }
+    return(number,number_num);
+}
+Console.WriteLine(hoge("123abc"));
