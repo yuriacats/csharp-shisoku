da78706d302ce53370b72c94ac8c8b4ccd1e229e
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-03-28 12:03:46 +0000

    lexのテストを完成させリファクタリングした

--- shisoku/Lex.cs
+++ shisoku/Lex.cs
@@ -1,109 +1,89 @@
 using System;
 namespace shisoku;
 public abstract record Token();
-public abstract record TokenSymble():Token;
-public record TokenNumber(int Number):Token;
-public record TokenPlus():TokenSymble;
-public record TokenSlash():TokenSymble;
-public record TokenMinus():TokenSymble;
-public record TokenAsterisk():TokenSymble;
-public record TokenStartSection():Token;
-public record TokenEndSection():Token;
-public class lexer{
+public abstract record TokenSymble() : Token;
+public record TokenNumber(int Number) : Token;
+public record TokenPlus() : TokenSymble;
+public record TokenSlash() : TokenSymble;
+public record TokenMinus() : TokenSymble;
+public record TokenAsterisk() : TokenSymble;
+public record TokenStartSection() : Token;
+public record TokenEndSection() : Token;
+public class lexer
+{
 
 
-public static List<Token> lex(String input){
-    List<string> raw_tokens= new List<string>{}; 
-    int x = 0;
-    while(input.Length >= 1){
-        if(input[x] == '+'){
-            raw_tokens.Add(input[..x]);
-            raw_tokens.Add("+");
-            input = input[(x+1)..];
-            x = 0;
-        }else if(input[x] == '-'){
-            raw_tokens.Add(input[..x]);
-            raw_tokens.Add("-");
-            input = input[(x+1)..];
-            x = 0;
+    public static List<Token> lex(String input)
+    {
+        List<Token> tokens = new List<Token> { };
+        while (input.Length >= 1)
+        {
+            if (input[0] == '+')
+            {
+                tokens.Add(new TokenPlus());
+                input = input[(1)..];
+            }
+            else if (input[0] == '-')
+            {
+                tokens.Add(new TokenMinus());
+                input = input[(1)..];
 
-        }else if(input[x] == '/'){
-            raw_tokens.Add(input[..x]);
-            raw_tokens.Add("/");
-            input = input[(x+1)..];
-            x = 0;
-        }else if(input[x] == '*'){
-            raw_tokens.Add(input[..x]);
-            raw_tokens.Add("*");
-            input = input[(x+1)..];
-            x = 0;
-        }else if(input[x] == '('){
-            raw_tokens.Add(input[..x]);
-            raw_tokens.Add("(");
-            input = input[(x+1)..];
-            x = 0;
+            }
+            else if (input[0] == '/')
+            {
+                tokens.Add(new TokenSlash());
+                input = input[(1)..];
+            }
+            else if (input[0] == '*')
+            {
+                tokens.Add(new TokenAsterisk());
+                input = input[(1)..];
+            }
+            else if (input[0] == '(')
+            {
+                tokens.Add(new TokenStartSection());
+                input = input[(1)..];
 
-        }else if(input[x] == ')'){
-            raw_tokens.Add(input[..x]);
-            raw_tokens.Add(")");
-            input = input[(x+1)..];
-            x = 0;
+            }
+            else if (input[0] == ')')
+            {
+                tokens.Add(new TokenEndSection());
+                input = input[(1)..];
 
-        }else{
-            if(input.Length <= x+1){
-                raw_tokens.Add(input);
+            }
+            else if (input[0] - '0' >= 0 && input[0] - '0' < 10)
+            {
+                (int target, int len) = lexInt(input);
+                tokens.Add(new TokenNumber(target));
+                input = input[(len)..];
+            }
+            else
+            {
+                throw new Exception("不正な文字が入力されています");
+            }
+        }
+        return tokens;
+    }
+    /// 文字列を受け取って、数字としてトーカナイズできるところまでを読み込み、値を返す
+    /// 読み込めない時は　０文字読めて０を返す
+    public static (int, int) lexInt(String input)
+    {
+        int number_num = 0;
+        int number = 0;
+        foreach (char i in input)
+        {
+            int digit = i - '0';
+            if (0 <= digit && digit < 10)
+            {
+                number = number * 10 + digit;
+                number_num++;
+            }
+            else
+            {
                 break;
             }
-            x++;
         }
+        return (number, number_num);
     }
-    List<Token> tokens = new List<Token>{};
-    foreach(string raw_token in raw_tokens){
-        switch(raw_token){
-            case string  i when raw_token == "+" :
-                tokens.Add(new TokenPlus());
-                break;
-            case string  i when raw_token == "-" :
-                tokens.Add(new TokenMinus());
-                break;
-            case string  i when raw_token == "*" :
-                tokens.Add(new TokenAsterisk());
-                break;
-            case string  i when raw_token == "/" :
-                tokens.Add(new TokenSlash());
-                break;
-            case string  i when raw_token == "(" :
-                tokens.Add(new TokenStartSection());
-                break;
-            case string  i when raw_token == ")" :
-                tokens.Add(new TokenEndSection());
-                break;
-            case string  i when raw_token == "" :
-                break;
-            default:
-                (int target, int _) = lexInt(raw_token);
-                Token t_n = new TokenNumber(target);
-                tokens.Add(t_n);
-                break;
-        }
-    }
-    return tokens;
-}
-
-public static (int,int) lexInt(String input){
-    int number_num=0;
-    int number = 0;
-    foreach (char i in input){
-        int digit = i - '0';
-        if (0 <= digit && digit < 10){
-            number = number* 10 + digit;
-            number_num++;
-        }
-        else{
-            break;
-        }
-    }
-    return(number,number_num);
-}
 }
 
