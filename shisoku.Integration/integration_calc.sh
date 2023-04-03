ef8e2e7f3177ec02d149e54bd284bb9d0a1c6e04
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-04-03 11:34:18 +0000

    make integration testcase

--- shisoku.Integration/integration_calc.sh
+++ shisoku.Integration/integration_calc.sh
@@ -1,8 +1,26 @@
 set -eu
 function main(){
-    test $(dotnet run --project shisoku -- --exp 1+1) -eq 2
-    test $(dotnet run --project shisoku -- --exp 1*1) -eq 1 
-    test $(dotnet run --project shisoku -- --exp "(1+1)*3" ) -eq 6
+    run_test_case 1+1 2
+    run_test_case 1*1 1 
+    run_test_case "(1+1)*3" 6
+    run_test_case "aaaa" error
 }
 
+function run_test_case(){
+    local input=($1)
+    local expected=($2)
+
+    local output=$(dotnet run --project shisoku -- --exp ${input})
+    local error_code=$?
+    if [ ${output} = ${expected} ];then
+        echo "${input} == ${expected}です"
+        true
+    elif [ ${expected} = "error" ] && [ ${error_code} -eq '0' ];then
+        echo "想定通りに異常終了しました"
+        true
+    else
+        echo "${input} == ${expected}ではありません"
+        false
+    fi
+}
 main
