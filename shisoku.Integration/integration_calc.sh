34ca761e32a405f69389b87ff638b26def8ef139
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-04-04 14:48:28 +0000

    fix 統合テストでFailケースが一つでもある場合全ての統合テストケースを動かした後に異常終了するようにする

--- shisoku.Integration/integration_calc.sh
+++ shisoku.Integration/integration_calc.sh
@@ -1,25 +1,35 @@
-set -eu
 function main(){
+    local exit_all=0
     run_test_case "足し算" 1+1 2 
+    exit_all=$((exit_all+$?))
     run_test_case "掛け算" 1*1 1 
+    exit_all=$((exit_all+$?))
     run_test_case "（）付きの計算式" "(1+1)*3" 6
+    exit_all=$((exit_all+$?))
     run_test_case "割算が優先" "1+6/3" 3
+    exit_all=$((exit_all+$?))
     #run_test_case "（）の後に（）が来ている" "(1+1)(3)" error
+    #exit_all=$((exit_all+$?))
     # なぜ上が正しくないかというとパースした後に全てのTokenが使い切れる構文木が生成されないから。 
     # Tokenが余っていたら弾く
     # TODO 本体コードを修正する
     run_test_case "空白は全て無視するよって正しい計算式が空白で区切られても問題ない" "1+1 +2" 4 
+    exit_all=$((exit_all+$?))
     run_test_case "(が終了していない" "(1+1" error
+    exit_all=$((exit_all+$?))
     run_test_case "文字列を入れると失敗する" "aaaa" error
+    exit_all=$((exit_all+$?))
     run_test_case "演算記号のみを入れると失敗する" "---" error
+    exit_all=$((exit_all+$?))
     run_test_case "演算記号のみを入れると失敗する" "1-2" -1 
-
+    exit_all=$((exit_all+$?))
+    return ${exit_all}
 }
 
 function run_test_case(){
-    local case_name=("$1")
-    local input=("$2")
-    local expected=("$3")
+    local case_name="$1"
+    local input="$2"
+    local expected="$3"
 
     local output
     output=$(dotnet run --no-build --project shisoku -- --exp "${input}" 2> /dev/null)
