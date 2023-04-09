21a9d1acaafb146f2e62633c85e00bc1d18957d1
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-04-09 12:04:08 +0000

    セミコロンの判定を分割した

--- shisoku/ParseStatement.cs
+++ shisoku/ParseStatement.cs
@@ -8,33 +8,30 @@ public class ParseStatement
 
     private static (Statement, Token[]) parseStatement(Token[] tokens)
     {
-        switch (tokens)
+        var (statement, token) = tokens switch
         {
-            case [TokenConst, ..]:
-                return parseConst(tokens);
-            default:
-                return parseExpressionStatement(tokens);
+            [TokenConst, ..] => parseConst(tokens),
+            _ => parseExpressionStatement(tokens)
+        };
+
+        if (token is [TokenSemicolon, .. var rest2])
+        {
+            return (statement, rest2);
         }
+
+        throw new Exception($"Token undefined: {token}");
     }
     private static (Statement, Token[]) parseExpressionStatement(Token[] tokens)
     {
         var (expression, rest) = ParseExpression.parse(tokens);
-        if (rest is [TokenSemicolon, .. var rest2])
-        {
-            return (new AstExpression(expression), rest2);
-        }
-        throw new Exception($"Token undefined: {rest}");
+        return (new AstExpression(expression), rest);
     }
     private static (Statement, Token[]) parseConst(Token[] tokens)
     {
         if (tokens is [TokenConst, TokenIdentifier(var exprName), TokenEqual, .. var rest2])
         {
             (var expression, var rest) = ParseExpression.parse(rest2);
-            if (rest is [TokenSemicolon, ..])
-            {
-                return (new AstConst(exprName, expression), rest);
-            }
-            throw new Exception($"Token undefined: {rest}");
+            return (new AstConst(exprName, expression), rest);
         }
         throw new Exception($"Token undefined: {tokens}");
     }
