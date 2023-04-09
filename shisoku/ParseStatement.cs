4a84a451425709358c0b817f402afde29b3f20cb
yuria <yuria.soft0615@gmail.com>
2023-04-09 21:18:51 +0900

    ParseStatementクラスの作成 (#32)
    * Statementクラスの作成
    
    * rename Parser
    
    * rename Parser
    
    * セミコロンの判定を分割した
    
    * セミコロンの判定を分割した
    
    * fix

---
+++ shisoku/ParseStatement.cs
@@ -0,0 +1,38 @@
+namespace shisoku;
+public class ParseStatement
+{
+    public static (Statement, Token[]) parse(Token[] tokens)
+    {
+        return parseStatement(tokens);
+    }
+
+    private static (Statement, Token[]) parseStatement(Token[] tokens)
+    {
+        var (statement, token) = tokens switch
+        {
+            [TokenConst, ..] => parseConst(tokens),
+            _ => parseExpressionStatement(tokens)
+        };
+
+        if (token is not [TokenSemicolon, ..])
+        {
+            throw new Exception($"Token undefined: {token}");
+        }
+
+        return (statement, token[1..]);
+    }
+    private static (Statement, Token[]) parseExpressionStatement(Token[] tokens)
+    {
+        var (expression, rest) = ParseExpression.parse(tokens);
+        return (new AstExpression(expression), rest);
+    }
+    private static (Statement, Token[]) parseConst(Token[] tokens)
+    {
+        if (tokens is [TokenConst, TokenIdentifier(var exprName), TokenEqual, .. var rest_token])
+        {
+            (var expression, var rest) = ParseExpression.parse(rest_token);
+            return (new AstConst(exprName, expression), rest);
+        }
+        throw new Exception($"Token undefined: {tokens}");
+    }
+}
