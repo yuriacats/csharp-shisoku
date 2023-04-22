504f5807d696f594a383e2ff33a1323bcdadbece
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-04-22 09:01:49 +0000

    Change test case names

--- shisoku.Tests/CalcTest.cs
+++ shisoku.Tests/CalcTest.cs
@@ -6,104 +6,104 @@ using shisoku;
 public class CalcTest
 {
     [Fact]
-    public void calcInputOnlyNumberExpression()
+    public void NumberEvaluates()
     {
         var expectedValue = new IntValue(12);
         var value = shisoku.CalcExpression.Calc(new NumberExpression(12), new VariableEnvironment());
         Assert.Equal<Value>(expectedValue, value);
     }
     [Fact]
-    public void calcInputAdd()
+    public void AddEvaluates()
     {
         var expectedValue = new IntValue(12);
         var value = shisoku.CalcExpression.Calc(new AddExpression(new NumberExpression(5), new NumberExpression(7)), new VariableEnvironment());
         Assert.Equal<Value>(expectedValue, value);
     }
     [Fact]
-    public void calcInputSubExpression()
+    public void SubEvaluates()
     {
         var expectedValue = new IntValue(12);
         var value = shisoku.CalcExpression.Calc(new SubExpression(new NumberExpression(16), new NumberExpression(4)), new VariableEnvironment());
         Assert.Equal<Value>(expectedValue, value);
     }
     [Fact]
-    public void calcInputMulExpression()
+    public void MulEvaluates()
     {
         var expectedValue = new IntValue(12);
         var value = shisoku.CalcExpression.Calc(new MulExpression(new NumberExpression(3), new NumberExpression(4)), new VariableEnvironment());
         Assert.Equal<Value>(expectedValue, value);
     }
     [Fact]
-    public void calcInputDivExpression()
+    public void DivEvaluates()
     {
         var expectedValue = new IntValue(12);
         var value = shisoku.CalcExpression.Calc(new DivExpression(new NumberExpression(24), new NumberExpression(2)), new VariableEnvironment());
         Assert.Equal<Value>(expectedValue, value);
     }
     [Fact]
-    public void calcInputEqual()
+    public void EqualEvaluatesToTrueWhenInputsAreSameNumbers()
     {
         var expectedValue = new BoolValue(true);
         var value = shisoku.CalcExpression.Calc(new EqualExpression(new NumberExpression(12), new NumberExpression(12)), new VariableEnvironment());
         Assert.Equal<Value>(expectedValue, value);
     }
     [Fact]
-    public void calcInputEqualFailed()
+    public void EqualEvaluatesToFalseWhenInputsAreDifferentNumbers()
     {
         var expectedValue = new BoolValue(false);
         var value = shisoku.CalcExpression.Calc(new EqualExpression(new NumberExpression(11), new NumberExpression(12)), new VariableEnvironment());
         Assert.Equal<Value>(expectedValue, value);
     }
     [Fact]
-    public void calcInputEqualTrue()
+    public void EqualEvaluatesToTrueWhenBothBooleansAreTrue()
     {
         var expectedValue = new BoolValue(true);
         var value = shisoku.CalcExpression.Calc(new EqualExpression(new BoolExpression(true), new BoolExpression(true)), new VariableEnvironment());
         Assert.Equal<Value>(expectedValue, value);
     }
     [Fact]
-    public void calcInputEqualFalse()
+    public void EqualEvaluatesToTrueWhenBothBooleansAreFalse()
     {
         var expectedValue = new BoolValue(true);
         var value = shisoku.CalcExpression.Calc(new EqualExpression(new BoolExpression(false), new BoolExpression(false)), new VariableEnvironment());
         Assert.Equal<Value>(expectedValue, value);
     }
     [Fact]
-    public void calcInputEqualTrueFalse()
+    public void EqualEvaluatesToFalseWhenFalseAndTrueAreGiven()
     {
         var expectedValue = new BoolValue(false);
         var value = shisoku.CalcExpression.Calc(new EqualExpression(new BoolExpression(false), new BoolExpression(true)), new VariableEnvironment());
         Assert.Equal<Value>(expectedValue, value);
     }
     [Fact]
-    public void calcInputEqualFalseTrue()
+    public void EqualEvaluatesToFalseWhenTrueAndFalseAreGiven()
     {
         var expectedValue = new BoolValue(false);
         var value = shisoku.CalcExpression.Calc(new EqualExpression(new BoolExpression(true), new BoolExpression(false)), new VariableEnvironment());
         Assert.Equal<Value>(expectedValue, value);
     }
     [Fact]
-    public void calcInputEqualOtherTypes()
+    public void EqualExpressionDoesNotEvaluateWhenDiffrentTypeArgmentsAreGiven()
     {
         Assert.Throws<Exception>(() => shisoku.CalcExpression.Calc(new EqualExpression(new BoolExpression(true), new NumberExpression(12)), new VariableEnvironment()));
     }
     [Fact]
-    public void calcInputAddCannotBool()
+    public void AddExpressionDoesNotEvaluateWhenBooleanTypeArgmentsAreGiven()
     {
         Assert.Throws<Exception>(() => shisoku.CalcExpression.Calc(new AddExpression(new BoolExpression(true), new BoolExpression(true)), new VariableEnvironment()));
     }
     [Fact]
-    public void calcInputSubCannotBool()
+    public void SubExpressionDoesNotEvaluateWhenBooleanTypeArgmentsAreGiven()
     {
         Assert.Throws<Exception>(() => shisoku.CalcExpression.Calc(new SubExpression(new BoolExpression(true), new BoolExpression(true)), new VariableEnvironment()));
     }
     [Fact]
-    public void calcInputMulCannotBool()
+    public void MulExpressionDoesNotEvaluateWhenBooleanTypeArgmentsAreGiven()
     {
         Assert.Throws<Exception>(() => shisoku.CalcExpression.Calc(new MulExpression(new BoolExpression(true), new BoolExpression(true)), new VariableEnvironment()));
     }
     [Fact]
-    public void calcInputDivCannotBool()
+    public void DivExpressionDoesNotEvaluateWhenBooleanTypeArgmentsAreGiven()
     {
         Assert.Throws<Exception>(() => shisoku.CalcExpression.Calc(new DivExpression(new BoolExpression(true), new BoolExpression(true)), new VariableEnvironment()));
     }
