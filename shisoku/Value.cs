20ab2ba1b33eebf85cc701962b2082f4b7956bd4
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-05-15 11:30:22 +0000

    add functionValue

--- shisoku/Value.cs
+++ shisoku/Value.cs
@@ -8,3 +8,7 @@ public record BoolValue(bool Value) : Value
 {
     public override string ToString() => Value.ToString();
 };
+public record FunctionValue(List<string> argumentNames, Statement[] body) : Value
+{
+    public override string ToString() => $"FunctionValue({string.Join(", ", argumentNames)})";
+};
