21ea49fe0ef48794c2bc145e97605eddd736c653
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-05-22 10:32:03 +0000

    Make CallExpression

--- shisoku/CalcExpression.cs
+++ shisoku/CalcExpression.cs
@@ -74,6 +74,29 @@ public class CalcExpression
                 {
                     return new FunctionValue(argumentNames, body);
                 }
+            case CallExpression(var arguments, var function):
+                {
+                    var newEnv = env.WithNewContext();
+                    var targetValue = Calc(function, newEnv);
+                    switch (targetValue)
+                    {
+                        case FunctionValue(var argumentNames, var body):
+                            for (int i = 0; i < arguments.Length; i++)
+                            {
+                                if (argumentNames.Contains(arguments[i].Item1))
+                                {
+                                    newEnv.Add(arguments[i].Item1, Calc(arguments[i].Item2, newEnv));
+                                }
+                                else
+                                {
+                                    throw new Exception($"{arguments[i].Item1} is not defined. in {argumentNames}");
+                                }
+                            }
+                            return CalcFunctionBody.Calc(body, newEnv);
+                        default:
+                            throw new Exception($"Method not implemented.");
+                    }
+                }
             default:
                 throw new Exception($"Evaluation Error:{input}");
         }
